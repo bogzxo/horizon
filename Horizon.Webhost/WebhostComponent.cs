b8adcf92fc299f8866ed3c3bd457d8f79efee845
bogzxo <bogdanpechyorin@gmail.com>
2024-04-10 06:52:24 -0700

    Create WebHost project and implement component template

---
+++ Horizon.Webhost/WebhostComponent.cs
@@ -0,0 +1,31 @@
+﻿using Horizon.Core;
+using Horizon.Core.Components;
+
+namespace Horizon.Webhost;
+
+public class WebhostComponent : IGameComponent
+{
+    public bool Enabled { get; set; }
+    public string Name { get; set; } = "Webost Component";
+    public Entity Parent { get; set; }
+
+    public void Initialize()
+    {
+        
+    }
+
+    public void Render(float dt, object? obj = null)
+    {
+        
+    }
+
+    public void UpdatePhysics(float dt)
+    {
+        
+    }
+
+    public void UpdateState(float dt)
+    {
+        
+    }
+}
