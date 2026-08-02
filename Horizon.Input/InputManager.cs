b2343e91e9d9700e8241d508500f6022bc21bb4a
bogzxo <bogdanpechyorin@gmail.com>
2026-08-02 20:41:54 +0200

    InputManager listens to Enabled!! vFINAL

--- Horizon.Input/InputManager.cs
+++ Horizon.Input/InputManager.cs
@@ -108,6 +108,7 @@ namespace Horizon.Input
         /// </summary>
         private void SwapBuffers(float _)
         {
+            if (!Enabled) return;
             for (int i = 0; i < Peripherals.Length; i++)
                 Peripherals[i].SwapBuffers();
 
@@ -119,6 +120,7 @@ namespace Horizon.Input
         /// </summary>
         private void AggregateInputs(float dt)
         {
+            if (!Enabled) return;
             for (int i = 0; i < Peripherals.Length; i++)
                 Peripherals[i].AggregateData(dt);
         }
